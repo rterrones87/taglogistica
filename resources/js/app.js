44920bd20f6fbbbb560241ca6c09be93ef7a25f5
Rodolfo Terrones <rterrones87@gmail.com>
2026-07-24 12:20:51 -0600

    api url

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,42 @@
 //import './bootstrap';
-import '../css/app.css';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import axios from 'axios';
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-
+import "../css/app.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 import swalPlugin from "./plugins/dialogs";
-import permissionDirective from './directives/permission';
+import permissionDirective from "./directives/permission";
 
 //axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-axios.defaults.baseURL = 'https://sistema.taglogistica.com/api/';
+axios.defaults.baseURL = "https://taglogistica.devtru.online/api/";
 
 //axios.defaults.withCredentials = true
 
 // Interceptor para agregar el token en cada petición
-axios.interceptors.request.use(config => {
+axios.interceptors.request.use(
+  (config) => {
     try {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     } catch (error) {
-        console.error('Error al obtener el token:', error);
+      console.error("Error al obtener el token:", error);
     }
     return config;
-}, error => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  },
+);
 
 const app = createApp(App);
 app.use(router);
 app.use(swalPlugin);
-app.directive('permission', permissionDirective);
-app.component('VueDatePicker', VueDatePicker);
+app.directive("permission", permissionDirective);
+app.component("VueDatePicker", VueDatePicker);
 app.config.globalProperties.$axios = axios;
-app.mount('#app');
-
+app.mount("#app");
